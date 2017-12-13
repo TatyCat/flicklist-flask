12cfd29216c157506397feed53bc909a0f7e2ba3
TatyCat <williams.tatyanna@gmail.com>
2017-12-12 23:18:35 -0500

    Studio 1 - Initialized project to display two random movie day titles

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['DEBUG'] = True
+# displays runtime errors in the browser, too
 
 @app.route("/")
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
-
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
-    content += "<li>" + movie + "</li>"
+    content += "<li>" + str(movie[0]) + "</li>"
     content += "</ul>"
 
-    # TODO: pick another random movie, and display it under
-    # the heading "<h1>Tommorrow's Movie</h1>"
+    # 2nd random movie for the next day
+    content += "<h1>Tommorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + str(movie[1]) + "</li>"
+    content += "</ul>"
 
     return content
 
+
+#Randomly chooses one of the movies and returns it:
 def get_random_movie():
-    # TODO: make a list with at least 5 movie titles
-    # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie_list = ["Air Space", "Robin", "Green Mile", "Movie Life", "Joker"]
+    rand_movie = random.sample(movie_list, 2)
+    return rand_movie
 
 
 app.run()
